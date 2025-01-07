7b2718e0468c6db9082c424d5782e33e67c085c5
Facundototo <facutoto666@gmail.com>
2025-01-07 17:38:07 -0300

    'add' argument finished, new functions load and save Tasks for JSON local file

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+const path = require('path');
+
 //mycli <add> <description>
 //mycli <update> <id_task>
 //mycli <delete> <id_task>
@@ -7,23 +10,43 @@
 //mycli <mark-done> <id_task>
 //mycli <list> [done,todo,in-progress]
 
+const FILE_PATH = path.join(__dirname, 'tasks.json');;
+
 const args = process.argv.slice(2);     //i extract the first two because they are the paths.
 
 // -- help argument here. --
 
-if(args.length < 2 && (args[0] !== 'list')) console.log('syntax error, check "help"');
-else options();
+options();
 
 function options(){
     switch(args[0]){
         case 'add':
-            //-- create a task in JSON file with id,description(which is args[1]),status,createdAt,updatedAt --
+            let tasks = loadTasks();
+
+            tasks.push({
+                id: tasks.length+1,
+                description: args.splice(1).join(' '),
+                status: 'todo',
+                createdAt: new Date().toLocaleString(),
+                updatedAt: null
+            })
+
+            saveTasks(tasks);
+
             break;
     }
 }
 
+function loadTasks(){
+    if(fs.existsSync(FILE_PATH)){
+        const data = fs.readFileSync(FILE_PATH,'utf8')
+        return JSON.parse(data);
+    }
+    return [];
+}
 
-
-
+function saveTasks(data){
+    fs.writeFileSync(FILE_PATH,JSON.stringify(data),'utf8');
+}
 
 
